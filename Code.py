56b5ee7b4f145f1ba81d61dd6c74845789b79b8f
elmehdielbachiri <el-mehdi.el-bachiri4@etu.univ-lorraine.fr>
2020-12-08 22:53:15 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,23 @@
 #Author: El Mehdi El Bachiri
 #Date 23/11/2020
-# Is git working ?
+
+
+import torch
+import torch.nn as nn
+from random import shuffle
+import pandas as pd
+from random import shuffle
+
+data = pd.read_csv('/Users/mac/Desktop/Projet Machine Learning/Data/Radar_Traffic_Counts.csv',sep=',') 
+
+data.drop(['Time Bin'],axis=1,inplace=True)
+data.sort_values(['location_name','Year','Month','Day','Hour','Minute'],inplace=True)
+
+print(data[:1000])
+
+
+#TIMES SERIES avec donnees POUR CHAQUE (Location,Direction,date (donnees chaque heure) 
+
+## First Model CNN
+
+
