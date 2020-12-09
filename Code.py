b82bf7d13a700e36f25ed91d0a07de90a884d9a6
elmehdielbachiri <el-mehdi.el-bachiri4@etu.univ-lorraine.fr>
2020-12-09 21:15:18 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,19 +1,32 @@
 #Author: El Mehdi El Bachiri
 #Date 23/11/2020
 
-
+import numpy as np
 import torch
 import torch.nn as nn
 from random import shuffle
 import pandas as pd
 from random import shuffle
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('/Users/mac/Desktop/Projet Machine Learning/Data/Radar_Traffic_Counts.csv',sep=',') 
 
 data.drop(['Time Bin'],axis=1,inplace=True)
-data.sort_values(['location_name','Direction','Year','Month','Day','Hour','Minute'],inplace=True)
+date1=data[['Year','Month','Day','Hour']]
+data.drop('Month',axis=1,inplace=True)
+data.drop('Day',axis=1,inplace=True)
+data.drop('Hour',axis=1,inplace=True)
+data.drop('Minute',axis=1,inplace=True)
+data.drop('Day of Week',axis=1,inplace=True)
+data.drop('location_longitude',axis=1,inplace=True)
+data.drop('location_latitude',axis=1,inplace=True)
 
 
+data[['Year']]=pd.to_datetime(date1,unit='D')
+data=data.rename(columns={'Year':'Date'})
+data=data.groupby(by=['location_name','Direction','Date'],as_index=False)['Volume'].sum()
+data.sort_values(['location_name','Direction','Date'],inplace=True)
+data.head()
 
 names=data['location_name'].unique().tolist()
 directions=['SB','NB','WB','EB','None']
@@ -24,6 +37,35 @@ test = data.loc[data.location_name==names[0]][data.Direction==directions[0]]
 ##location and direction should be strings here
 def GetTimeseries(location,direction):
     tsdata=data.loc[data.location_name==location][data.Direction==direction]
-    
+    tsdata=pd.Series(np.array(tsdata['Volume']),index=tsdata['Date'])
+    tsdata.plot
+    plt.show()
+    return tsdata
+
+
 ## First Model CNN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
