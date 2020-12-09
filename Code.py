9941361ec23114253f7c5bbbb7ec4454fc5223a8
elmehdielbachiri <el-mehdi.el-bachiri4@etu.univ-lorraine.fr>
2020-12-09 19:49:10 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -11,13 +11,19 @@ from random import shuffle
 data = pd.read_csv('/Users/mac/Desktop/Projet Machine Learning/Data/Radar_Traffic_Counts.csv',sep=',') 
 
 data.drop(['Time Bin'],axis=1,inplace=True)
-data.sort_values(['location_name','Year','Month','Day','Hour','Minute'],inplace=True)
+data.sort_values(['location_name','Direction','Year','Month','Day','Hour','Minute'],inplace=True)
 
-print(data[:1000])
 
+
+names=data['location_name'].unique().tolist()
+directions=['SB','NB','WB','EB','None']
+
+test = data.loc[data.location_name==names[0]][data.Direction==directions[0]]
 
 #TIMES SERIES avec donnees POUR CHAQUE (Location,Direction,date (donnees chaque heure) 
-
+##location and direction should be strings here
+def GetTimeseries(location,direction):
+    tsdata=data.loc[data.location_name==location][data.Direction==direction]
+    
 ## First Model CNN
-
 
