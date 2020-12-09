9ad08997896e86e29bf455ad451ce32be0f7016a
elmehdielbachiri <el-mehdi.el-bachiri4@etu.univ-lorraine.fr>
2020-12-09 21:21:47 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -38,15 +38,40 @@ test = data.loc[data.location_name==names[0]][data.Direction==directions[0]]
 def GetTimeseries(location,direction):
     tsdata=data.loc[data.location_name==location][data.Direction==direction]
     tsdata=pd.Series(np.array(tsdata['Volume']),index=tsdata['Date'])
-    tsdata.plot
+    tsdata.plot()
     plt.show()
     return tsdata
 
 
 ## First Model CNN
 
-
-
+class TimeCNN(nn.Module):
+    def __init__(self):
+        super(TimeCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(8)
+        )
+        self.fc1 = nn.Linear(in_features=64*8, out_features=128)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=128, out_features=32)
+        self.fc3 = nn.Linear(in_features=32, out_features=1)
+ 
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
 
 
 
