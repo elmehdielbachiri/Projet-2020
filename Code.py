58c9848d3f1dcd29e083179c7a949345c36aa4db
elmehdielbachiri <el-mehdi.el-bachiri4@etu.univ-lorraine.fr>
2020-12-10 20:27:57 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -37,11 +37,37 @@ test = data.loc[data.location_name==names[0]][data.Direction==directions[0]]
 
 # PARAMETERS:
 # Sliding Step : 2 weeks
-A=24
+A=24*7
 # Prediction Window: predict 1 week
-B=24 #(Maximum 10 jours pour avoir condition relative aux couches B<128*2=256)
+B=24 #(Maximum 2 jours pour avoir condition relative aux couches B<128*2=256)
 # Base training window: (8 weeks here)
-m = 24*28
+m = 24*28*2
+
+# FOR : A=24*7
+# Prediction Window: predict 1 week
+# B=24 #(Maximum 2 jours pour avoir condition relative aux couches B<128*2=256)
+# Base training window: (8 weeks here)
+# m = 24*28*2
+# epoch 0 loss 13.663952358 testMSE 0.036261350
+# epoch 1 loss 11.580940861 testMSE 0.106750794
+# epoch 2 loss 11.368167730 testMSE 0.053207424
+# epoch 3 loss 12.217421100 testMSE 0.059360106
+# epoch 4 loss 11.638190819 testMSE 0.018149979
+# epoch 5 loss 11.520276275 testMSE 0.058282465
+# epoch 6 loss 11.319544181 testMSE 0.039052293
+# epoch 7 loss 11.287821383 testMSE 0.053247068
+# epoch 8 loss 11.165210446 testMSE 0.075795271
+# epoch 9 loss 11.256984541 testMSE 0.036187522
+# epoch 10 loss 11.282688415 testMSE 0.051304504
+# epoch 11 loss 11.216889381 testMSE 0.056534965
+# epoch 12 loss 11.033790175 testMSE 0.048409667
+# epoch 13 loss 11.458225170 testMSE 0.064952679
+# epoch 14 loss 11.130934507 testMSE 0.031871408
+# epoch 15 loss 12.486707686 testMSE 0.042964790
+# epoch 16 loss 12.092693646 testMSE 0.036532227
+# epoch 17 loss 11.621955955 testMSE 0.063966967
+# epoch 18 loss 11.553644409 testMSE 0.046707377
+# epoch 19 loss 11.023357280 testMSE 0.061495472 
 
 
 #TIMES SERIES avec donnees POUR CHAQUE (Location,Direction,date (donnees chaque heure) 
@@ -76,12 +102,12 @@ class TimeCNN(nn.Module):
         )
         
         #Linear Layer 1
-        self.fc1 = nn.Linear(in_features=64*8, out_features=B*2)
+        self.fc1 = nn.Linear(in_features=64*8, out_features=128)
         self.drop = nn.Dropout2d(0.25)
         #Linear Layer 2
-        self.fc2 = nn.Linear(in_features=B*2, out_features=B)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
         #Linear Layer 3
-        self.fc3 = nn.Linear(in_features=B, out_features=B)
+        self.fc3 = nn.Linear(in_features=64, out_features=B)
  
     def forward(self, x):
         out = self.layer1(x)
